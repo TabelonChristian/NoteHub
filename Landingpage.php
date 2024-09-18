3fc732b88ed168fe12e7d286f39293157647ad4e
TabelonChristian <157966517+TabelonChristian@users.noreply.github.com>
2024-09-18 23:13:16 +0800

    Landingpage.php

--- Landingpage.php
+++ Landingpage.php
@@ -5,28 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Notehub</title>
     <link rel="stylesheet" href="Css/nav.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
 <body>
 <div class="coverpage"> 
     <div class="thecontent">
     <div class="left">
-    <img src="Image/Bg.avif" alt="">
-    <div class="content1">
+        <figure>
+            <img src="Image/Bg.avif" alt="">
+            <figcaption>Caption for the background image</figcaption>
+        </figure>
+        <div class="content1">
             <div class="trans">
                 <h3>Words have power, write them down, make them real.</h3>
             </div>
-    </div>
+        </div>
     </div>
     <div class="right">
     <div class="content2">
         <div class="Logo">
-                <a href="Landingpage.php" class="log">Note<span class="Orange">hub</span></a>
-                <p>Where notes take center stage. Join us as we explore the power of effective note-taking. Let's make every word count.</p>
+            <a href="Landingpage.php" class="log">Note<span class="Orange">hub</span></a>
+            <p>Where notes take center stage. Join us as we explore the power of effective note-taking. Let's make every word count.</p>
         </div>
-        <div class="form">
-            <a href="register.php" class="sign">Sign Up</a>
-            <a href="login.php" class="Log">Log in</a>
-        </div>
+        <nav class="form">
+            <ul>
+                <li><a href="register.php" class="sign">Sign Up</a></li>
+                <li><a href="login.php" class="Log">Log in</a></li>
+            </ul>
+        </nav>
     </div>
     </div>
     </div>
