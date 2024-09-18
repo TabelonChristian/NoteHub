00f3a824bc666214d202eedc78312281898a3a24
TabelonChristian <157966517+TabelonChristian@users.noreply.github.com>
2024-09-18 23:41:09 +0800

    addinguser.php

--- addinguser.php
+++ addinguser.php
@@ -1,12 +1,11 @@
 <?php
 include_once "dbconnection.php";
 
-$conn = connectDB();
+$conn = connectToDatabase();
 
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
-    $data = htmlspecialchars($data);
     return $data;
 }
 
@@ -36,6 +35,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $emailErr = "Email is required";
     } else {
         $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email address";
+        }
     }
 
     if (empty($_POST["username"])) {
@@ -57,22 +59,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($firstname !== "" && $lastname !== "" && $dob !== "" && $email !== "" && $username !== "" && $password !== "") {
         try {
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-            $stmt = $conn->prepare("INSERT INTO usertable (user_fname, user_lname, user_bdate, user_age, user_email, user_username, user_password) VALUES (:firstname, :lastname, :dob, :age, :email, :username, :password)");
-
-            $stmt->bindParam(':firstname', $firstname);
-            $stmt->bindParam(':lastname', $lastname);
-            $stmt->bindParam(':dob', $dob);
-            $stmt->bindParam(':age', $age);
-            $stmt->bindParam(':email', $email);
+            $stmt = $conn->prepare("SELECT * FROM usertable WHERE user_username = :username OR user_email = :email");
             $stmt->bindParam(':username', $username);
-            $stmt->bindParam(':password', $hashed_password);
-
+            $stmt->bindParam(':email', $email);
             $stmt->execute();
 
-            header("Location: login.php");
-            exit();
+            if ($stmt->rowCount() > 0) {
+                $usernameErr = "Username or email address already exists";
+            } else {
+                $stmt = $conn->prepare("INSERT INTO usertable (user_fname, user_lname, user_bdate, user_age, user_email, user_username, user_password) VALUES (:firstname, :lastname, :dob, :age, :email, :username, :password)");
+
+                $stmt->bindParam(':firstname', $firstname);
+                $stmt->bindParam(':lastname', $lastname);
+                $stmt->bindParam(':dob', $dob);
+                $stmt->bindParam(':age', $age);
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':username', $username);
+                $stmt->bindParam(':password', $hashed_password);
+
+                $stmt->execute();
+
+                header("Location: login.php");
+                exit();
+            }
         } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
+            error_log("Error: " . $e->getMessage());
+            echo "Error: Unable to process request";
         }
     }
 }
