75c7676a3002b52d6e86e4a7c2bd1dc0ff080faf
TabelonChristian <157966517+TabelonChristian@users.noreply.github.com>
2024-09-18 22:06:17 +0800

    dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,17 +1,17 @@
 <?php
-function connectDB() {
-    $host = "localhost";
-    $dbname = "notes";
-    $username = "root";
-    $password = "";
-
-    try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
-    } catch (PDOException $e) {
-        error_log("Connection failed: " . $e->getMessage());
-        return null;
+function connectToDatabase() {
+    $servername = getenv ('DB_HOST') ?: 'localhost';
+    $username = getenv  ('DB_USERNAME') ?: 'root';
+    $password = getenv('DB_PASSWORD')?: '';
+    $database = getenv('DB_DATABASE') ?: 'note_db';
+    $port = getenv('DB_PORT')?: '3306';
+ $conn = new PDO("mysql:host=$servername;dbname=$database;port=$port",$username, $password);
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ return $conn;
+ } catch(PDOException $e) {
+     die ("Connection Failed: " . $e->getmessage());
+ }
     }
-}
+    $conn = connectToDatabase();
+
 ?>
